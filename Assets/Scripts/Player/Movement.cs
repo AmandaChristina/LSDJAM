a80d8d6cbd0ac28fb641412bced2ba8e5c3723b4
AmandaChristina <amanda.christina.n@gmail.com>
2023-01-11 23:27:47 -0300

    Maquina de estado
    Transição entre os estados andar e nadar completa
    Ainda apresenta bugs: nadar e levantar ficam alterando as vezes

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    CharacterController controller;
+    
+    enum States { MOVE, SWIM, UP,STOP} //Cria as opções
+    [SerializeField]
+    States _state; // cria a variável que vai receber a opção atual
 
+    GameObject waterObj;
+    CharacterController controller;
     Vector3 move;
 
+    [SerializeField]
+    bool isMoving, isSwimming, isStoping;
+
+    //Vars of Move
     float hMove;
     float zMove;
     [SerializeField]
@@ -15,31 +24,93 @@ public class Movement : MonoBehaviour
     [SerializeField]
     float gravity;
 
+    //Vars of Swim
+
+
     void Start()
     {
-        controller = gameObject.GetComponent<CharacterController>(); 
+        waterObj = GameObject.Find("Water");
+        controller = gameObject.GetComponent<CharacterController>();
+        isMoving = true;
+
     }
 
 
     void Update()
     {
-        Walking();
-        Falling();
 
-        
+        #region Máquina de Estado
+        switch (_state)
+        {
+            case States.MOVE:
+                Walking();
+                Falling();
+                break;
 
+            case States.SWIM:
+                Swimming();
+                break;
+
+            case States.UP:
+                StartCoroutine(GetOutWater());
+                break;
+
+            case States.STOP:
+                print("Estou parado");
+                break;
+
+            default:
+                Walking();
+                Falling();
+                break;
+        }
+        #endregion
+
+        //Verificar se está nadando
+        if(waterObj != null)
+        {
+            float yPlayer = transform.position.y;
+            float yWater = waterObj.transform.position.y;
+
+            float waterDistance = yWater - yPlayer;
+
+            if (waterDistance > -controller.height / 2)
+            {
+                isMoving = false;
+                isSwimming = true;
+            }
+            else
+            {
+                isSwimming = false;
+                
+            }
+        }
+
+        if (isSwimming) SwitchStates(States.SWIM);
+        else if (!isSwimming && !isMoving) SwitchStates(States.UP);
+        else if (isMoving)SwitchStates(States.MOVE);
+
+
+    }
+
+    void GettingAxis()
+    {
+        hMove = Input.GetAxis("Horizontal");
+        zMove = Input.GetAxis("Vertical");
     }
 
     void Walking()
     {
-        hMove = Input.GetAxis("Horizontal");
-        zMove = Input.GetAxis("Vertical");
+
+        GettingAxis();
+
 
         move = new Vector3(hMove, 0, zMove).normalized;
         move = transform.TransformDirection(move);
 
-
         controller.Move(move * Time.deltaTime * speed);
+
+
     }
 
     void Falling()
@@ -53,8 +124,26 @@ public class Movement : MonoBehaviour
 
     void Swimming()
     {
+        Walking();
+        transform.Rotate(Vector3.right * -Input.GetAxis("Mouse Y") * Time.deltaTime * 300f);
+        print("estou nadando");
 
     }
 
-   
+    IEnumerator GetOutWater()
+    {
+        transform.rotation = Quaternion.identity; //deixa perfeitamente zerado
+        transform.rotation = Quaternion.Euler(Vector3.up * 90f);
+        print("Levantando");
+        yield return new WaitForSeconds(0.1f);
+        isMoving = true;
+        SwitchStates(States.MOVE);
+
+    }
+
+    void SwitchStates(States state)
+    {
+        _state = state;
+    }
+
 }
